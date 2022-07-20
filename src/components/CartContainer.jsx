e0499e8d930522b872f0f48c1c5b5b95a2c2428b
phong47047 <phong47047@donga.edu.vn>
2022-07-20 15:56:12 +0700

    Cart + Finish

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 import { RiRefreshFill } from 'react-icons/ri';
 import { motion } from 'framer-motion';
-import { BiMinus, BiPlus } from 'react-icons/bi';
 import { useStateValue } from '~/context/StateProvider';
 import { actionType } from '~/context/reducer';
 import EmptyCart from '../img/emptyCart.svg';
+import { CartItem } from '.';
+import { useEffect } from 'react';
 const CartContainer = () => {
-  const [{ cartShow, cartItems }, dispatch] = useStateValue();
-
+  const [{ cartShow, cartItems, user }, dispatch] = useStateValue();
+  const [tot, setTot] = useState([]);
+  const [flag, setFlag] = useState([]);
   const showCart = () => {
     dispatch({
       type: actionType.SET_CART_SHOW,
       cartShow: !cartShow,
     });
+  };
+
+  useEffect(() => {
+    let totalPrice = cartItems.reduce(function (accumulator, item) {
+      return accumulator + item.qty * item.price;
+    }, 0);
+    setTot(totalPrice);
+    console.log(tot);
+  }, [tot, flag]);
+  const clearCart = () => {
+    dispatch({
+      type: actionType.SET_CARTITEMS,
+      cartItems: [],
+    });
+    localStorage.setItem('cartItems', JSON.stringify([]));
   };
   return (
     <motion.div
@@ -34,6 +51,7 @@ const CartContainer = () => {
           hover:shadow-md cursor-pointer text-textColor 
           text-base
           "
+          onClick={clearCart}
         >
           Clear <RiRefreshFill />
         </motion.p>
@@ -45,36 +63,14 @@ const CartContainer = () => {
           <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
             {/* cart item */}
             {cartItems &&
+              cartItems.length > 0 &&
               cartItems.map((item) => (
-                <div
+                <CartItem
                   key={item.id}
-                  className="w-full p-1 px-2 rounded-lg bg-cardItem flex items-center gap-2"
-                >
-                  <img
-                    src={item?.imageURL}
-                    className="w-20 h-20 max-w-[60px] rounded-full object-contain"
-                    alt=""
-                  />
-                  {/* name section */}
-                  <div className="flex flex-col gap-2">
-                    <p className="text-base text-gray-50">{item?.title}</p>
-                    <p className="text-sm block text-gray-300 font-semibold">
-                      ${item?.price}
-                    </p>
-                  </div>
-                  {/* button section */}
-                  <div className="group flex items-center gap-2 ml-auto cursor-pointer">
-                    <motion.div whileTap={{ scale: 0.75 }}>
-                      <BiMinus className="text-gray-50" />
-                    </motion.div>
-                    <p className="w-5 h-5 rounded-sm bg-cardBg text-gray-50 flex items-center justify-center">
-                      {item?.qty}
-                    </p>
-                    <motion.div whileTap={{ scale: 0.75 }}>
-                      <BiPlus className="text-gray-50" />
-                    </motion.div>
-                  </div>
-                </div>
+                  item={item}
+                  setFlag={setFlag}
+                  flag={flag}
+                />
               ))}
           </div>
 
@@ -82,35 +78,48 @@ const CartContainer = () => {
           <div className="w-full flex-1 bg-cardTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-400 text-lg">Tổng phụ</p>
-              <p className="text-gray-400 text-lg">$9.5</p>
+              <p className="text-gray-400 text-lg">${tot}</p>
             </div>
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-400 text-lg">Phí Vận Chuyển & Phụ Thu</p>
-              <p className="text-gray-400 text-lg">$2.5</p>
+              <p className="text-gray-400 text-lg">$4.99</p>
             </div>
 
             <div className="w-full border-b border-gray-600 my-2"></div>
 
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-200 text-lg font-semibold">Tổng</p>
-              <p className="text-gray-200 text-lg font-semibold">$12</p>
+              <p className="text-gray-200 text-lg font-semibold">
+                ${tot + 2.5}
+              </p>
             </div>
 
-            <motion.button
-              whileTap={{ scale: 0.8 }}
-              type="button"
-              className="w-full py-4 rounded-full bg-gradient-to-tr from-orange-400 to-orange-500 text-gray-50 text-xl my-2
+            {user ? (
+              <motion.button
+                whileTap={{ scale: 0.8 }}
+                type="button"
+                className="w-full py-4 rounded-full bg-gradient-to-tr from-orange-400 to-orange-500 text-gray-50 text-xl my-2
             hover:shadow-lg transition-all duration-150 ease-out"
-            >
-              Thanh toán
-            </motion.button>
+              >
+                Thanh toán
+              </motion.button>
+            ) : (
+              <motion.button
+                whileTap={{ scale: 0.8 }}
+                type="button"
+                className="w-full py-4 rounded-full bg-gradient-to-tr from-orange-400 to-orange-500 text-gray-50 text-xl my-2
+            hover:shadow-lg transition-all duration-150 ease-out"
+              >
+                Đăng nhập đã Đại Vương
+              </motion.button>
+            )}
           </div>
         </div>
       ) : (
         <div className="w-full h-full flex flex-col items-center justify-center gap-6">
           <img src={EmptyCart} className="w-300" alt="" />
-          <p className="text-xl text-textColor font-semibold">
-            Click vào giỏ hàng bên cạnh sản phẩm đi đại vương ơi
+          <p className="text-xl text-textColor font-semibold mx-8">
+            Chọn sản phẩm đi đại vương ơi
           </p>
         </div>
       )}
